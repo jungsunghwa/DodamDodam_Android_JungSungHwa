ff5fb8997c051f6d9f172b9d43f97951cbcf80ee
Dong_Hyck_Na <43693001+agvx124@users.noreply.github.com>
2019-08-28 20:47:01 +0900

    회원가입 서버연동

---
+++ app/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/network/client/RegisterClient.java
@@ -0,0 +1,55 @@
+package kr.hs.dgsw.smartschool.dodamdodam.network.client;
+
+import android.content.Context;
+
+import io.reactivex.Single;
+import kr.hs.dgsw.b1nd.service.B1ndService;
+import kr.hs.dgsw.b1nd.service.interfaces.OnRegisterSuccessListener;
+import kr.hs.dgsw.b1nd.service.retrofit2.response.register.StudentRegisterRequest;
+import kr.hs.dgsw.b1nd.service.retrofit2.response.register.TeacherRegisterRequest;
+import kr.hs.dgsw.b1nd.service.service.B1ndRegister;
+import kr.hs.dgsw.smartschool.dodamdodam.Constants;
+import kr.hs.dgsw.smartschool.dodamdodam.Model.member.Student;
+
+
+public class RegisterClient extends NetworkClient {
+
+    public RegisterClient(Context context) {
+        super(context);
+        B1ndService.setHostURL(Constants.DEFAULT_HOST);
+    }
+
+    public Single<String> studentRegister(StudentRegisterRequest request) {
+        return Single.create(observer -> new B1ndRegister().studentRegister(request, new OnRegisterSuccessListener() {
+            @Override
+            public void onSuccess(int status, String message) {
+                if (status == 200) {
+                    observer.onSuccess(message);
+                }
+            }
+
+            @Override
+            public void onFail(Throwable throwable, String message) {
+                throwable.printStackTrace();
+                observer.onError(new Throwable(throwable.getMessage()));
+            }
+        }));
+    }
+
+    public Single<String> teacherRegister(TeacherRegisterRequest request) {
+        return Single.create(observer -> new B1ndRegister().teacherRegister(request, new OnRegisterSuccessListener() {
+            @Override
+            public void onSuccess(int status, String message) {
+                if (status == 200) {
+                    observer.onSuccess(message);
+                }
+            }
+
+            @Override
+            public void onFail(Throwable throwable, String message) {
+                throwable.printStackTrace();
+                observer.onError(new Throwable(throwable.getMessage()));
+            }
+        }));
+    }
+}
---
+++ app/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/viewmodel/RegisterViewModel.java
@@ -0,0 +1,75 @@
+package kr.hs.dgsw.smartschool.dodamdodam.viewmodel;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
+import kr.hs.dgsw.b1nd.service.retrofit2.response.register.StudentRegisterRequest;
+import kr.hs.dgsw.b1nd.service.retrofit2.response.register.TeacherRegisterRequest;
+import kr.hs.dgsw.smartschool.dodamdodam.network.client.RegisterClient;
+
+public class RegisterViewModel extends AndroidViewModel {
+
+    private final MutableLiveData<String> isSuccess = new MutableLiveData<>();
+    private final MutableLiveData<String> errorMessage = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> loading = new MutableLiveData<>();
+    private RegisterClient registerClient;
+    private CompositeDisposable disposable;
+
+    public RegisterViewModel(Application application) {
+        super(application);
+        registerClient = new RegisterClient(application);
+        disposable = new CompositeDisposable();
+    }
+
+    public MutableLiveData<String> getIsSuccess() {
+        return isSuccess;
+    }
+
+    public MutableLiveData<String> getErrorMessage() {
+        return errorMessage;
+    }
+
+    public MutableLiveData<Boolean> getLoading() {
+        return loading;
+    }
+
+    public void studentRegister(StudentRegisterRequest request) {
+        loading.setValue(true);
+        disposable.add(registerClient.studentRegister(request).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribeWith(new DisposableSingleObserver<String>(){
+            @Override
+            public void onSuccess(String s) {
+                isSuccess.setValue(s);
+                loading.setValue(false);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                errorMessage.setValue(e.getMessage());
+                loading.setValue(false);
+            }
+        }));
+    }
+
+    public void teacherRegister(TeacherRegisterRequest request) {
+        loading.setValue(true);
+        disposable.add(registerClient.teacherRegister(request).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribeWith(new DisposableSingleObserver<String>(){
+            @Override
+            public void onSuccess(String s) {
+                isSuccess.setValue(s);
+                loading.setValue(false);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                errorMessage.setValue(e.getMessage());
+                loading.setValue(false);
+            }
+        }));
+    }
+}
